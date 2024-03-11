7649453ca49d40620cd75ffbc0ab7b13b15e4b64
Vinay <meshramvinay007@gmail.com>
2024-03-11 20:40:33 +0530

    Api created successfully

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 4000;
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.ucjgxf5.mongodb.net/UserDB`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/sign-in", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const encryptedPassword = await bcrypt.hash(password, salt);
+  try {
+    const user = await User.insertMany([
+      { name, email, password: encryptedPassword },
+    ]);
+
+    const payload = { user: { id: user.id, email: user.email } };
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    res.json({ success: true, message: "Sign-in successful", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Failed to add user" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+    const payload = { user: { id: user.id, email: user.email } };
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({ success: true, message: "Logged in successful", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+app.listen(port, () => console.log("Server listening to port", port));
